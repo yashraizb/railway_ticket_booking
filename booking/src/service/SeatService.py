8cd227c07959ef65ef2fbcbabc0b48bca9bb13c4
Yash Rai <yashraizb@gmail.com>
2025-12-29 00:48:55 +0530

    SeatService: added book seat

--- booking/src/service/SeatService.py
+++ booking/src/service/SeatService.py
@@ -1,6 +1,9 @@
 from booking.src.strategy.seat.SeatStrategy import SeatStrategy
 from booking.src.factory.SeatFactory import SeatFactory
 from booking.src.domain.JourneyDetailHandler import JourneyDetailHandler
+from booking.models import Seat, Train, Station, RouteStation
+from django.db.models import Q, Count
+from django.db import transaction
 
 
 SEAT_STRATEGY = "simple"
@@ -23,3 +26,63 @@ class SeatService:
             "train": journeyDetails.get_train(),
         }
         return self.seatStrategy.get_available_seats(data)
+    
+    def book_seat(self, bookSerializer):
+        train_number = bookSerializer.validated_data["train_number"]
+        source_name = bookSerializer.validated_data["source"]
+        destination_name = bookSerializer.validated_data["destination"]
+        journey_date = bookSerializer.validated_data["journey_date"]
+        coach_type = bookSerializer.validated_data["coach_type"]
+
+        try:
+            train = Train.objects.get(number=train_number)
+            source = Station.objects.get(name=source_name)
+            destination = Station.objects.get(name=destination_name)
+        except Train.DoesNotExist:
+            raise Exception("Train not found")
+        except Station.DoesNotExist:
+            raise Exception("Station not found")
+
+        src_stn_seq = (
+            RouteStation.objects.filter(train=train, station=source).first().sequence
+        )
+
+        dest_stn_seq = (
+            RouteStation.objects.filter(train=train, station=destination)
+            .first()
+            .sequence
+        )
+
+        list_seats = Seat.objects.filter(
+            Q(destination_station_sequence__gt=src_stn_seq)
+            & Q(destination_station_sequence__lte=dest_stn_seq),
+            train=train,
+            journey_date=journey_date,
+            coach__coach_type=coach_type,
+            status="available",
+        )
+
+        available_seats = (
+            list_seats.values("coach__coach_type", "coach_number", "seat_number")
+            .annotate(cnt=Count("id"))
+            .filter(cnt=(dest_stn_seq - src_stn_seq))
+        )
+
+        if not available_seats.exists():
+            raise Exception("No available seats")
+
+        seat_number = list_seats.first().seat_number
+
+        with transaction.atomic():
+
+            Seat.objects.select_for_update().filter(
+                Q(destination_station_sequence__gt=src_stn_seq)
+                & Q(destination_station_sequence__lte=dest_stn_seq),
+                train=train,
+                journey_date=journey_date,
+                coach__coach_type=coach_type,
+                status="available",
+                seat_number=seat_number,
+            ).update(status="booked")
+        
+        return seat_number
--- booking/views.py
+++ booking/views.py
@@ -111,71 +111,17 @@ class BookSeatView(APIView):
                 status=400,
             )
 
-        train_number = bookSerializer.validated_data["train_number"]
-        source_name = bookSerializer.validated_data["source"]
-        destination_name = bookSerializer.validated_data["destination"]
-        journey_date = bookSerializer.validated_data["journey_date"]
-        coach_type = bookSerializer.validated_data["coach_type"]
-
-        try:
-            train = Train.objects.get(number=train_number)
-            source = Station.objects.get(name=source_name)
-            destination = Station.objects.get(name=destination_name)
-        except Train.DoesNotExist:
-            return JsonResponse({"message": "Train not found"}, status=404)
-        except Station.DoesNotExist:
-            return JsonResponse({"message": "Station not found"}, status=404)
-
-        src_stn_seq = (
-            RouteStation.objects.filter(train=train, station=source).first().sequence
-        )
-
-        dest_stn_seq = (
-            RouteStation.objects.filter(train=train, station=destination)
-            .first()
-            .sequence
-        )
-
-        list_seats = Seat.objects.filter(
-            Q(destination_station_sequence__gt=src_stn_seq)
-            & Q(destination_station_sequence__lte=dest_stn_seq),
-            train=train,
-            journey_date=journey_date,
-            coach__coach_type=coach_type,
-            status="available",
-        )
-
-        available_seats = (
-            list_seats.values("coach__coach_type", "coach_number", "seat_number")
-            .annotate(cnt=Count("id"))
-            .filter(cnt=(dest_stn_seq - src_stn_seq))
-        )
-
-        if not available_seats.exists():
-            return JsonResponse({"message": "No seats available"}, status=404)
-
-        seat_number = list_seats.first().seat_number
-
-        with transaction.atomic():
-
-            Seat.objects.select_for_update().filter(
-                Q(destination_station_sequence__gt=src_stn_seq)
-                & Q(destination_station_sequence__lte=dest_stn_seq),
-                train=train,
-                journey_date=journey_date,
-                coach__coach_type=coach_type,
-                status="available",
-                seat_number=seat_number,
-            ).update(status="booked")
+        seatService = SeatService()
+        seat_number = seatService.book_seat(bookSerializer)
 
         return JsonResponse(
             {
                 "message": "Seat booked successfully",
                 "seat_number": seat_number,
-                "coach_type": coach_type,
-                "train_number": train_number,
-                "source": source_name,
-                "destination": destination_name,
+                "coach_type": bookSerializer.validated_data["coach_type"],
+                "train_number": bookSerializer.validated_data["train_number"],
+                "source": bookSerializer.validated_data["source"],
+                "destination": bookSerializer.validated_data["destination"],
             },
             status=200,
         )
